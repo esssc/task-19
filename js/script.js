060d18b08f9c1116e36990b893413ffaf90737e3
esssc <37302254+esssc@users.noreply.github.com>
2018-03-14 13:46:24 +0200

    put validation

--- js/script.js
+++ js/script.js
@@ -1,18 +1,33 @@
 $(function () {
 
-	alert("Hello! I am an alert box!!");
+	//alert("Hello! I am an alert box!!");
 	$('#register-form').validate({
 		rules:{
 			email: {
 				required: true,
 				email: true
 			},
-			
+			name: {
+				required:true
+				
+			},
+			phone: {
+				required:true,
+				number:true
+			}
+
 		},
 		messages: {
 			email: {
 				required: 'Prasome irašyti elektroninį paštą.',
 				email: 'Prasome irašyti <em>teisingą</em> elektroninį paštą.'
+			},
+			name: {
+				required: 'Irasykite savo varda'
+			},
+			phone: {
+				required: 'Telefono numeris',
+				number: 'Iveskite numeri'
 			}
 		}
 	});
